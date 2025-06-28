ba08626dac9050a495bf683c9225e820afb80c5e
Bigbilly-github <abiolatwitter97@gmail.com>
2025-06-28 21:51:33 +0100

    created the sales page, created and styled salesdatadisplay, and salesoverviewchart componenets.

---
+++ src/components/sales/salesstatsdisplay.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import Stats from "../global/stats";
+import {motion} from 'framer-motion'
+import { CreditCard, DollarSign, ShoppingCart, TrendingUp } from 'lucide-react';
+import Salesoverviewchart from './Salesoverviewchart';
+
+const Salesstatsdisplay = () => {
+    const salesStats = {
+	totalRevenue: "$1,234,567",
+	averageOrderValue: "$78.90",
+	conversionRate: "3.45%",
+	salesGrowth: "12.3%",
+};
+
+  return (
+      <section className="max-w-7xl mx-auto py-6 px-4 lg:px-8 ">
+            <motion.div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+                >
+
+                     
+                <Stats name='Total Revenue' Icon={DollarSign} value={salesStats.totalRevenue} color='#6366F1' />
+					<Stats
+						name='Avg. Order Value'
+						Icon={ShoppingCart}
+						value={salesStats.averageOrderValue}
+						color='#10B981'
+					/>
+					<Stats
+						name='Conversion Rate'
+						Icon={TrendingUp}
+						value={salesStats.conversionRate}
+						color='#F59E0B'
+					/>
+					<Stats name='Sales Growth' Icon={CreditCard} value={salesStats.salesGrowth} color='#EF4444' />
+			
+                  
+
+
+
+            </motion.div>
+            <Salesoverviewchart/>
+                 
+               <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+                      
+                     
+
+                    </div>
+                    
+
+        </section>
+  )
+}
+
+export default Salesstatsdisplay
--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import Sidebar from "../components/global/sidebar";
+import Header from "../components/global/header";
+import Salesstatsdisplay from '../components/sales/salesstatsdisplay'
+
 const Sales = () => {
   return (
-    <div>sales</div>
+     <div className='flex-1 flex items-start overflow-auto relative z-10'>
+             <Sidebar/>
+            <div className="w-full">
+                <Header title="Sales"/>
+                <main>
+                   <Salesstatsdisplay/>
+                   
+                </main>
+        
+            </div>
+        
+
+      </div>
   )
 }
 
