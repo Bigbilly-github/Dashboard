432182112b526e6a4eae1fe4bd3007fdfff906e4
Bigbilly-github <abiolatwitter97@gmail.com>
2025-06-29 16:49:11 +0100

    created and styled userretention component

--- src/components/analytics/Analyticsdisplay.jsx
+++ src/components/analytics/Analyticsdisplay.jsx
@@ -5,6 +5,7 @@ import Analyticscard from "./Analyticscard";
 import ChannelPerformance from "./Channelperfomance";
 import ProductPerformance from "./Productperfomance";
 import RevenueChart from "./Revenuechart";
+import UserRetention from "./Userretention";
 
 const Analyticsdisplay = () => {
   return (
@@ -14,6 +15,7 @@ const Analyticsdisplay = () => {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
         <ChannelPerformance/>
         <ProductPerformance/>
+        <UserRetention/>
        
          
       </div>
