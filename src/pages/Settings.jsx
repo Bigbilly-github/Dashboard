ddc1078baa9b57531241011a14e6d305da5bff7c
Bigbilly-github <abiolatwitter97@gmail.com>
2025-06-29 17:45:06 +0100

    created and styled the notifications and toggleswitch components

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/global/header";
 import Sidebar from "../components/global/sidebar";
+import Notifications from "../components/settings/Notifcations";
 import Profile from "../components/settings/profile";
 const Settings = () => {
 	return (
@@ -9,6 +10,7 @@ const Settings = () => {
                 <Header title="Settings"/>
                  <main className='max-w-4xl mx-auto py-6 px-4 lg:px-8'>
                    <Profile />
+                   <Notifications/>
                    
                 </main>
 
