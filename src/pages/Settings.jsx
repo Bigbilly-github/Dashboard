be56a45137f0358855dd8428a0f121192be38940
Bigbilly-github <abiolatwitter97@gmail.com>
2025-06-29 17:39:37 +0100

    created components profile and settingsection

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import Header from "../components/global/header";
+import Sidebar from "../components/global/sidebar";
+import Profile from "../components/settings/profile";
 const Settings = () => {
-  return (
-    <div>Settings</div>
-  )
-}
+	return (
+     <div className='flex-1 flex items-start overflow-auto relative z-10'>
+             <Sidebar/>
+            <div className="w-full">
+                <Header title="Settings"/>
+                 <main className='max-w-4xl mx-auto py-6 px-4 lg:px-8'>
+                   <Profile />
+                   
+                </main>
 
+            </div>
+        
+
+      </div>
+
+	);
+};
 export default Settings
